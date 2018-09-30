649db82997c869987b05a2b6b5f4a9140a944b39
zrt11 <2487242402@qq.com>
2018-10-01 02:48:52 +0800

    Initial commit

---
+++ module02/src/com/java/modules/module02.java
@@ -0,0 +1,9 @@
+package com.java.modules;
+
+import java.util.ArrayList;
+
+public class module02 {
+    public static void main(String[] args) {
+        ArrayList arrayList=new ArrayList();
+    }
+}
---
+++ src/com/java/Idea.java
@@ -0,0 +1,17 @@
+package com.java;
+
+/**
+ * @author zhangruitao
+ *
+ * @date 2018 10
+ */
+public class Idea {
+    public static void main(String[] args) {
+        System.out.println("hello hello");
+    }
+    
+    public void test01(){
+        System.out.println("");
+    }
+
+}
